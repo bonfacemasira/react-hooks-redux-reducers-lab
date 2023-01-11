be268e71a859ee65e4d3886f2d1b7cd2896e6d88
Bonface Masira <bonfacemasira@gmail.com>
2023-01-11 10:05:21 +0300

    Done

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,29 @@
 export function manageFriends(state, action) {
   // your code here
+  // action = {
+  //   type: "friends/add",
+  //   payload: {
+  //     name: "Chrome Boi",
+  //     hometown: "NYC",
+  //     id: 1,
+  //   },
+  // };
+
+  // action = {
+  //   type: "friends/remove",
+  //   payload: 1,
+  // };
+
+  switch (action.type) {
+    case "friends/add":
+      return {
+        friends: [...state.friends, action.payload],
+      };
+    case "friends/remove":
+      return {
+        friends: state.friends.filter((friend) => friend.id!== action.payload),
+      };
+    default:
+      return state;
+  }
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -2,4 +2,14 @@ const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+  // action = {
+  //   type: "presents/increase",
+  // };
+
+  switch (action.type) {
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
 }
